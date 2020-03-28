a8fe21719b630cb8a0b90b65c42f661ab21f851c
Werdon4 <filipgajewski4@gmail.com>
2020-03-28 10:48:59 +0100

    Create user model
    User model with  validation function to store logins and passwords
    registered users.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const home = require('./routes/home');
 const express = require('express');
 const app = express();
 
-app.use('/', home)
+app.use('/', home);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
