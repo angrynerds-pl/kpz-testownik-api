28b89c4422dd779fe86d731e541ab6259012f156
Werdon4 <filipgajewski4@gmail.com>
2020-03-28 11:51:35 +0100

    Create endpoint to user register
    Endpoint create user from login and password from request body.
    Password will be hashed.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const users = require('./routes/users');
 const home = require('./routes/home');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/testownik')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
 app.use('/', home);
+app.use('/users', users);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
