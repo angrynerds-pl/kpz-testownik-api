96627f38057f01cb8a9ff162f7caa9c93746303d
werd0n4 <filipgajewski4@gmail.com>
2020-04-07 14:08:37 +0200

    Change in user model login to username
    Atribut name in user model has been changed to ensure compatibility with
    web. In app.js cors module has been added.

--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,10 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ login: req.body.login });
-    if (user) return res.status(400).send('User with this login already exists.'); 
+    let user = await User.findOne({ username: req.body.username });
+    if (user) return res.status(400).send('User with this username already exists.'); 
 
-    user = new User(_.pick(req.body, ['login', 'password']));
+    user = new User(_.pick(req.body, ['username', 'password']));
 
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
     await user.save();
 
     const token = user.generateAuthToken();
-    res.header('x-auth-token',token).send(_.pick(user, ['_id', 'login']));
+    res.header('x-auth-token',token).send(_.pick(user, ['_id', 'username']));
 });
 
 module.exports = router;
